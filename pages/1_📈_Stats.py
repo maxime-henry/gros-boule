ae3c38b27c4c483bf6c87f611e5153202439fc56
Maxime <maxime.3497@gmail.com>
2024-02-10 23:14:28 +0100

    Update 1_📈_Stats.py

--- pages/1_📈_Stats.py
+++ pages/1_📈_Stats.py
@@ -60,11 +60,9 @@ st.metric(label=f"Record du plus de squats en une session 👏", value=person_mo
 
 fig = px.histogram(data_frame=df, x="squats", title="Distribution des Squats")
 fig.update_layout(
-    width=600,  # Set the width of the plot
-    height=400,  # Set the height of the plot,
     xaxis_title="Squats",
     yaxis_title="Nombre de sessions")
-st.plotly_chart(fig)
+st.plotly_chart(fig, use_container_width=True)
 st.write("---")
 
 # Line plot of squats over time for each person
